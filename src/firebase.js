0cc5948723ca7e9252b55d1a5c4d7cff9e1564cf
Yash <itsyashu27@gmail.com>
2025-05-05 14:24:17 -0400

    firebase integration

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDO7cOWrzqn6AahfmkV08cs5arlZr4m89Y",
-  authDomain: "auctionspin-d40ff.firebaseapp.com",
-  projectId: "auctionspin-d40ff",
-  storageBucket: "auctionspin-d40ff.appspot.com", 
-  messagingSenderId: "396341428933",
-  appId: "1:396341428933:web:3411efdc0365cd42d2e6ac"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
